680d6883312b75d0b4ae3c7d5859d14e9f7b9ea4
duke1014 <duke.norsworthy@gmail.com>
2022-04-07 17:10:32 -0400

    added barebones of music section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home';
+import Music from './Music';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Home /> } />
+        <Route path="/music" element={ <Music /> } />
       </Routes>
   </BrowserRouter>
   );
