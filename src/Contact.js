a663a29e525bafd2dd468821805c47a516f97c3b
duke1014 <duke.norsworthy@gmail.com>
2022-04-09 00:21:49 -0400

    links added to contacts

--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import Navbar from './components/navbar'
 
-export default function 
-Contact() {
+export default function Contact() {
   return (
     <div>
         <Navbar />
-        Contact
+        <div>
+          Facebook
+        </div>
+        <div>
+          Instagram
+        </div>
+        <div>
+          Twitter
+        </div>
+        <div>
+          Twitch
+        </div>
+        <div>
+        <a href = "mailto: duke.norsworthy@gmail.com">Email</a>
+        </div>
     </div>
   )
 }
