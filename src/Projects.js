b454ee615deaf5f8e9d277705938e4ce9a4db1eb
duke1014 <duke.norsworthy@gmail.com>
2022-04-14 23:13:37 -0400

    added to projects

--- src/Projects.js
+++ src/Projects.js
@@ -5,7 +5,10 @@ export default function Projects() {
   return (
     <div>
         <Navbar />
-        Projects
+        To-do List: (link here)
+        Soundboard (link here)
+        Musician/Instrument List (Link here)
+
     </div>
   )
 }
